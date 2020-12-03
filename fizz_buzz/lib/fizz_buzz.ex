33f1d794f51ead1b324b343859d9ee5b8cbebac1
hedionegrao2 <hedionegrao@gmail.com>
2020-12-03 20:55:10 -0300

    ControlandoFluxo

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,7 +1,16 @@
 defmodule FizzBuzz do
- 
-  def build(file_neame) do
-    arquivo =  File.read(file_neame)
-    IO.inspect(arquivo)
+
+  def build(file_neme) do
+   # case File.read(file_neme) do
+   #   {:ok, result} -> result
+   #   {:error, reason} -> reason
+   # end
+   file_neme
+    |> File.read()
+    |> handle_file_read()
   end
+
+  def handle_file_read({:ok, result}),   do: result
+  def handle_file_read({:error, reason}),   do: reason
+
 end
