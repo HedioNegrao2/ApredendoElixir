98ff4db4c4bf3a1d6537afc82b258ef3c69df10b
hedionegrao2 <hedionegrao@gmail.com>
2020-12-02 23:17:19 -0300

    Iniciando

---
+++ linguagemElixir.ex
@@ -0,0 +1,5 @@
+#Interepolacao = " Strint  #{variavel}"
+# Concatenar "valor i" <> "valor2# "
+#is_ tab
+# rem() resto da divisao e div inteiro
+# None do modulo . nome da funcao String.downcase("Hedio")
