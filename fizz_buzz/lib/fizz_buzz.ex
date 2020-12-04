0fdb349a96f917ed9d04b6635c38cb778c7e3d97
hedionegrao2 <hedionegrao@gmail.com>
2020-12-03 22:54:18 -0300

    Incluindo o teste

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,22 +1,34 @@
 defmodule FizzBuzz do
-
   def build(file_neme) do
-   # case File.read(file_neme) do
-   #   {:ok, result} -> result
-   #   {:error, reason} -> reason
-   # end
-   file_neme
+    # case File.read(file_neme) do
+    #   {:ok, result} -> result
+    #   {:error, reason} -> reason
+    # end
+    file_neme
     |> File.read()
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
-    result
+  defp handle_file_read({:ok, result}) do
+    resultado =
+      result
       |> String.split(",")
-      |> Enum.map(&String.to_integer/1) #Esse & server para chamar um funcao que não é nossa e o /1 é a aridade
+      # Esse & server para chamar um funcao  e o /1 é a aridade
+      |> Enum.map(&converter_e_alterar_numero/1)
 
+    {:ok, resultado}
   end
 
-  def handle_file_read({:error, reason}),   do: "Erro ao ler o arquivo :#{reason}"
+  defp handle_file_read({:error, reason}), do: {:error, "Erro ao ler o arquivo : #{reason}"}
 
+  defp converter_e_alterar_numero(item) do
+    item
+    |> String.to_integer()
+    |> avaliar_numero()
+  end
+
+  defp avaliar_numero(numero) when rem(numero, 3) == 0 and rem(numero, 5) == 0, do: :fizzbuzz
+  defp avaliar_numero(numero) when rem(numero, 3) == 0, do: :fiz
+  defp avaliar_numero(numero) when rem(numero, 5) == 0, do: :buzz
+  defp avaliar_numero(numero), do: numero
 end
--- fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "Quando arquivo valido, retorne lisa" do
+      retornoEsperado = {:ok, [1, 2, :fiz, 4, :buzz, :fiz, :buzz, :fizzbuzz, :buzz, :fiz]}
+      assert FizzBuzz.build("numeros.txt") == retornoEsperado
+    end
+
+    test "Quando arquivo invalido, retorne erro" do
+      retornoEsperado = {:error, "Erro ao ler o arquivo : enoent"}
+      assert FizzBuzz.build("numero2s.txt") == retornoEsperado
+    end
   end
 end
