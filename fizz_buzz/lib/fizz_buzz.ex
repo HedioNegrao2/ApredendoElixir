b2968a2352f32dbbf23d4da96d4b0d187dbf90a8
hedionegrao2 <hedionegrao@gmail.com>
2020-12-03 21:51:07 -0300

    Converso lista em numero

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -10,7 +10,13 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}),   do: result
-  def handle_file_read({:error, reason}),   do: reason
+  def handle_file_read({:ok, result}) do
+    result
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1) #Esse & server para chamar um funcao que não é nossa e o /1 é a aridade
+
+  end
+
+  def handle_file_read({:error, reason}),   do: "Erro ao ler o arquivo :#{reason}"
 
 end
